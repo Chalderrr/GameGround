46c5f05bcb3ab4a2453d20630026660f30a85a9c
Liam Chalder <liam.chalder@laser.red>
2020-08-13 16:05:34 +0100

    Add deps, frame layout with html

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GameGround</title>
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body class="bg-gray-900 text-white">
+
+    <header class="border-b border-gray-800">
+        <nav class="container mx-auto flex items-center justify-between px-4 py-6">
+            <div class="flex items-center">
+                <a href="/">
+                    <img src="/laracasts-logo.svg" alt="laracasts" class="w-32 flex-none">
+                </a>
+
+                <ul class="flex ml-16 space-x-8">
+                    <li><a href="#" class="hover:text-gray-400">Games</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
+                    <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
+                </ul>
+            </div>
+
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text" name="" class="bg-gray-800 text-sm rounded-full focus:outline-none focus:shadow-outline px-3 py-1 w-64" placeholder="Search..." id="">
+                </div>
+                <div class="ml-6">
+                    <a href="#">
+                        <img src="/avatar.jpg" alt="avatar" class="rounded-full w-8">
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main class="py-8">
+        @yield('content')
+    </main>
+
+<footer class="border-t border-gray-800">
+    <div class="container mx-auto px-4 py-6">
+        Powered By <a href="#" class="underline hover:text-gray-400">IGDB Api</a>
+    </div>
+</footer>
+
+</body>
+</html>
